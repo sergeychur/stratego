be99a22e2e762b5bdb1008382cda23829d64f8dc
sergeychur <churicovser@mail.ru>
2018-12-10 02:12:03 +0300

    configured circleci

---
+++ CMakeFiles/stratego.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stratego.dir/main.cpp.o"
+  "stratego.pdb"
+  "stratego"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stratego.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
