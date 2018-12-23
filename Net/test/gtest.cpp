471e7817fbaa348018343d6bdbcffb48f2eb2b3e
Alex <alexsmekhunov@gmail.com>
2018-12-23 17:09:00 +0300

    net test updated

--- Net/test/gtest.cpp
+++ Net/test/gtest.cpp
@@ -2,11 +2,12 @@
 // Created by alex on 29.10.18.
 //
 
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
